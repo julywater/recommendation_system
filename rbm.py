16ed231e691053bd43c652e21b6dd7fda78ee9d5
julywater <tangyike455665@gmail.com>
2019-10-13 20:47:49 -0500

    initial change

---
+++ rbm.py
@@ -0,0 +1,14 @@
+mport numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+class RBM(object):
+
+        def __init__(self, visibleDimensions, epochs=20, hiddenDimensions=50, ratingValues=10, learningRate=0.001, batchSize=100):
+
+                    self.visibleDimensions = visibleDimensions
+                    self.epochs = epochs
+                    self.hiddenDimensions = hiddenDimensions
+                    self.ratingValues = ratingValues
+                    self.learningRate = learningRate
+                    self.batchSize = batchSize
