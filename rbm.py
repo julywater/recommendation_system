b48f5f8336e45365bf5b0d9607408d20b7a1a160
julywater <tangyike455665@gmail.com>
2019-11-05 17:01:36 -0600

    neural collabrative filtering

--- rbm.py
+++ rbm.py
@@ -1,14 +1,16 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.framework import ops
 
 class RBM(object):
-
-        def __init__(self, visibleDimensions, epochs=20, hiddenDimensions=50, ratingValues=10, learningRate=0.001, batchSize=100):
-
-                    self.visibleDimensions = visibleDimensions
-                    self.epochs = epochs
-                    self.hiddenDimensions = hiddenDimensions
-                    self.ratingValues = ratingValues
-                    self.learningRate = learningRate
-                    self.batchSize = batchSize
+	def __init__(self, visibleDimensions, hiddenDimensions=50, ratingValues=10, learningRate=0.001):
+		self.visibleDimensions = visibleDimensions
+        self.hiddenDimensions = hiddenDimensions
+        self.ratingValues = ratingValues
+        self.learningRate = learningRate
+		self.X = tf.placeholder(tf.float32, [None, self.visibleDimensions], name="X")
+	def init_parameters(self):
+		maxWeight = -4.0 * np.sqrt(6.0 / (self.hiddenDimensions + self.visibleDimensions))
+        self.weights = tf.Variable(tf.random_uniform([self.visibleDimensions, self.hiddenDimensions], minval=-maxWeight, maxval=maxWeight), tf.float32, name="weights")
+        self.hBias = tf.Variable(tf.zeros([self.hiddenDimensions], tf.float32,name="hBias"))
+        self.vBias = tf.Variable(tf.zeros([self.visibleDimensions],tf.float32,name="vBias"))
+    def gibbs_sampling(self):  
